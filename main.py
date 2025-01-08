b3541734d996b64a1a334f45adfaecbfebc94c75
wrathgewp <39569621+wrathgewp@users.noreply.github.com>
2025-01-08 17:36:39 +0100

    Aggiunto dizionario_utenti

---
+++ main.py
@@ -0,0 +1,24 @@
+'''
+Creare una lista di dizionari per rappresentare gli utenti della biblioteca.
+Ogni dizionario deve contenere le seguenti chiavi: 
+nome, cognome, libri_prestati (una lista di titoli di libri prestati).
+'''
+
+# Questo è un esempio di come potrebbe essere strutturato il dizionario degli utenti
+dizionario_utenti = {
+    'utente1': {
+        'nome': 'Mario',
+        'cognome': 'Rossi',
+        'libri_prestati': ['Il signore degli anelli', 'IT']
+    },
+    'utente2': {
+        'nome': 'Luca',
+        'cognome': 'Verdi',
+        'libri_prestati': ['Il nome della rosa', 'Il vecchio e il mare']
+    },
+    'utente3': {
+        'nome': 'Giulia',
+        'cognome': 'Bianchi',
+        'libri_prestati': ['Il signore degli anelli', 'Il nome della rosa']
+    }
+}
